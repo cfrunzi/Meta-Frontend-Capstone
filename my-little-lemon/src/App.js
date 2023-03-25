efdea7d8fe9a343bea1156f52149c839f64a1e5b
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-25 16:25:08 -0400

    added individual semantic components

--- my-little-lemon/src/App.js
+++ my-little-lemon/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Header from './Header'
+import Nav from "./Nav"
+import Main from "./Main"
+import Footer from "./Footer"
 
 function App() {
   return (
     <>
-    <header></header>
-    <nav></nav>
-    <main></main>
-    <footer></footer>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
     </>
   );
 }
